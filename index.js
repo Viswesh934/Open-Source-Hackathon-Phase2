f679b866c872b401a957efd498c1484609ca2213
sigireddy viswesh <98519767+Viswesh934@users.noreply.github.com>
2023-10-21 21:58:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -55,3 +55,45 @@ function run(lang, code, input) {
             console.error("Fetch error:", error);
         });
 }
+//download code
+
+const downloadButton = document.getElementById("review-button");
+
+downloadButton.addEventListener("click", () => {
+  const code = codebox.value;
+
+  if (code.length > 0) {
+    const blob = new Blob([code], { type: "text/plain" });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "code.txt";
+    a.click();
+    window.URL.revokeObjectURL(url);
+  } else {
+    alert("Please enter some code before downloading.");
+  }
+});
+
+const codeSampleButton = document.getElementById("code-sample-button");
+
+codeSampleButton.addEventListener("click", () => {
+  const selection = document.getElementById("lang");
+  const syntax = document.getElementById("syntax").value;
+  const lang = selection.value;
+
+  const requestOptions = {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  };
+
+  fetch(`https://api.codex.jaagrav.in/code-samples/${lang}/${syntax}`, requestOptions)
+    .then((response) => response.json())
+    .then((data) => {
+      const codebox = document.getElementById("code");
+      codebox.value = data.code;
+    })
+    .catch((error) => {
+      console.error("Fetch error:", error);
+    });
+});
